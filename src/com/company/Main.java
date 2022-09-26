2bad163578ca0abff25dd56e78c2ad45caeddb7c
sruthi-002 <sruthiramesh270@gmail.com>
2022-09-26 17:25:41 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.net.*;
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            URL url=new URL("http://localhost:8080/allemployee");
+
+            System.out.println("Protocol: "+url.getProtocol());
+            System.out.println("Host Name: "+url.getHost());
+            System.out.println("Port Number: "+url.getPort());
+            System.out.println("File Name: "+url.getFile());
+
+        }catch(Exception e){System.out.println(e);}
+
+    }
+}
